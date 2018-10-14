105a1dab33a03af3aabee43195a1a5d2f6a7580d
Ahmed Nergal <ahmed.nergal@gmail.com>
2018-10-14 22:25:13 +0200

    Added navbar, bug fixes, finished styling

--- application.py
+++ application.py
@@ -36,34 +36,40 @@ def index():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "GET":
-        return render_template("register.html")
-    elif request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        try:
-            db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
-                    {"username" : username, "password" : password})
-            db.commit()
-            return redirect("/search", code=303)
+    if 'username' not in session:
+        if request.method == "GET":
+            return render_template("register.html")
+        elif request.method == "POST":
+            username = request.form.get("username")
+            password = request.form.get("password")
+            try:
+                db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+                        {"username" : username, "password" : password})
+                db.commit()
+                return redirect("/search", code=303)
 
-        except:
-            return render_template("error.html", message="Username already exists!!!")
+            except:
+                return render_template("error.html", message="Username already exists!!!")
+    else:
+            return redirect("/", code=303)
 
 # Login Routes
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "GET":
-        return render_template("login.html")
-    elif request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        if db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username" : username, "password" : password}).rowcount != 0:
-            session['username'] = username
-            return redirect("/search", code=303)
-        else:
-            return render_template("error.html", message="Incorrect username or password!!")
+    if 'username' not in session:
+        if request.method == "GET":
+            return render_template("login.html")
+        elif request.method == "POST":
+            username = request.form.get("username")
+            password = request.form.get("password")
+            if db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username" : username, "password" : password}).rowcount != 0:
+                session['username'] = username
+                return redirect("/search", code=303)
+            else:
+                return render_template("error.html", message="Incorrect username or password!!")
+    else:
+        return redirect("/", code=303)
 
 # Logout Route
 
@@ -78,10 +84,12 @@ def logout():
 def search():
     if request.method == "GET":
         if 'username' in session:
-            return render_template("search.html")
+                username = session['username']
+                return render_template("search.html", username=username)
         else:
             return redirect("/login")
     elif request.method == "POST":
+        username = session['username']
         keyword = request.form.get("keyword")
         query = request.form.get("query")
         if query == "isbn":
@@ -93,24 +101,29 @@ def search():
         elif query == "year":
             results = db.execute("SELECT * FROM books WHERE year = :keyword", {"keyword" : keyword}).fetchall()
 
-        return render_template("results.html", results=results)
+        return render_template("results.html", results=results, username=username)
 
 # Book Route
 
 @app.route("/book/<int:book_id>")
 def book(book_id):
-    book = db.execute("SELECT * FROM books WHERE id = :id", {"id" : book_id}).fetchone()
-    reviews = db.execute("SELECT * FROM reviews JOIN users ON reviews.user_id = users.id WHERE book_id = :book_id", {"book_id" : book_id}).fetchall()
-    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key" : "DnUD36XqebyhLfdgeQav5Q", "isbns" : book.isbn})
-    goodreads_results = res.json()
-    average_score = goodreads_results["books"][0]["average_rating"]
-    review_count = goodreads_results["books"][0]["reviews_count"]
-    print(average_score)
-    print(review_count)
-    if book is None:
-        return render_template("error.html", message="Such book doesn't exist!!!")
+    if 'username' in session:
+        username = session['username']
+        book = db.execute("SELECT * FROM books WHERE id = :id", {"id" : book_id}).fetchone()
+        reviews = db.execute("SELECT * FROM reviews JOIN users ON reviews.user_id = users.id WHERE book_id = :book_id", {"book_id" : book_id}).fetchall()
+        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key" : "DnUD36XqebyhLfdgeQav5Q", "isbns" : book.isbn})
+        goodreads_results = res.json()
+        average_score = goodreads_results["books"][0]["average_rating"]
+        review_count = goodreads_results["books"][0]["reviews_count"]
+        print(average_score)
+        print(review_count)
+        if book is None:
+            return render_template("error.html", message="Such book doesn't exist!!!")
+        else:
+            return render_template("book.html", book=book, reviews=reviews, average_score=average_score, review_count=review_count, username=username)
+
     else:
-        return render_template("book.html", book=book, reviews=reviews, average_score=average_score, review_count=review_count)
+        return render_template("login.html")
 
 # Review Routes
 
@@ -122,11 +135,11 @@ def review(book_id):
         user_id = int(db.execute("SELECT id FROM users WHERE username = :username", {"username" : username}).fetchone()[0])
         print('The user id is: ' + str(user_id))
         if request.method == "GET":
-            return render_template("review.html", book=book)
+            return render_template("review.html", book=book, username=username)
         elif request.method == "POST":
             if db.execute("SELECT * FROM reviews WHERE user_id = :user_id AND book_id = :book_id",
                     {"user_id" : user_id, "book_id" : book_id }).rowcount != 0:
-                    return render_template("error.html", message="You reviewed this book before!!!")
+                    return render_template("error.html", username=username, message="You reviewed this book before!!!")
             else:
                 rating = request.form.get("rating")
                 review_title = request.form.get("review_title")
@@ -134,7 +147,9 @@ def review(book_id):
                 db.execute("INSERT INTO reviews (user_id, book_id, rating, review_title, review_body) VALUES (:user_id, :book_id, :rating, :review_title, :review_body)",
                             {"user_id" : user_id, "book_id" : book_id, "rating" : rating, "review_title" : review_title, "review_body" : review_body})
                 db.commit()
-                return redirect("/", code=303)
+                stringId = str(book_id)
+                redirectURL = "/book/" + stringId
+                return redirect(redirectURL, code=303)
 
 # ISBN API Route
 
